d2f9af7ffb59e7c5f959176f8a8fb76fca967735
Pedro Drumond <drumondpe@gmail.com>
2020-10-16 15:37:08 -0300

    funcao distribui mais cartas

--- EP_2020.2.py
+++ EP_2020.2.py
@@ -26,3 +26,24 @@ def jogo():
             outra_carta = True
         return outra_carta
 
+    def distribui_mais_carta(soma_jogador, soma_banco, outra_carta): #distribui mais uma carta caso não alcance 5
+        if soma_jogador <= 5:
+            #distribui mais uma carta para o jogador
+            carta_jogador_3 = baralho[4]
+            fatia = carta_jogador_3[0:2]
+            valor_carta_jogador_3 = dic_valores_cartas[fatia] #atribui o valor da carta buscando no dic lá de cima
+            soma_jogador += valor_carta_jogador_3 #recalcula a nova soma p/ jogador
+            print('O jogador recebeu a carta {} '.format(baralho[4]))
+            print('A somatória do jogador deu: {}'.format(soma_jogador))
+
+        if soma_banco <= 5:
+            #distribui mais uma carta para o banco
+            carta_banco_3 = baralho[5]
+            fatib = carta_banco_3[0:2]
+            valor_carta_banco_3 = dic_valores_cartas[fatib] #atribui o valor da carta buscando no dic lá de cima
+            soma_banco += valor_carta_banco_3 #recalcula a nova soma p/ banco
+            print('O banco recebeu a carta {} '.format(baralho[5]))
+            print('A somatória do banco deu: {}'.format(soma_banco))
+
+        return soma_jogador, soma_banco
+
